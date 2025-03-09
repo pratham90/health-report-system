e94c652abaf0bfd213a7ebbae94fa451ce997d55
pratham rathore <prathamrathore2004@gmail.com>
2025-03-10 01:04:02 +0530

    my

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,12 @@ import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
 
 
-<StrictMode>
+
   <BrowserRouter basename="/health-report-system">
       <App /> 
       
       </BrowserRouter>
   
-  </StrictMode>
+  
   
 )
