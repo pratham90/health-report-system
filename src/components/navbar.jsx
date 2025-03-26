6aac8771f33a4108d083bc1008299e567a9f0f17
pratham rathore <prathamrathore2004@gmail.com>
2025-03-26 15:49:54 +0530

    modified

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   const { isLoggedIn } = useAuth(); // Get login state
   return (
     <>
-      <div className="nav  bg-[#E5F5E0]  h-12 justify-between xl:justify-around  lg:px-10 md:h-15 px-5 md:px-5 py-5 items-center text-black flex ">
+      <div className="nav  bg-[#E5F5E0]  h-12 justify-between xl:justify-around  lg:px-10 md:h-15 px-5 md:px-5 py-5 items-center text-black flex overflow-hidden">
         <div className="md:hidden flex  items-center">
           <FontAwesomeIcon
             className="text-black h-6 w-6 cursor-pointer"
@@ -60,44 +60,49 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="flex gap-3 nav-items">
-            {isLoggedIn ? (
-          <Link to="/appointment" className="btn bg-gradient-to-r from-[#9beb9c] to-[#51ec99] rounded-3xl px-4 py-2 text-black ">
-            Appointment
-          </Link>
-        ) : (
-          <Link to="/signup" className="btn bg-gradient-to-r from-[#9beb9c] to-[#51ec99] px-4 py-2 rounded-3xl  text-blackfont-bold">
-            Sign-up
-          </Link>
-        )}
+          {isLoggedIn ? (
+            <Link
+              to="/appointment"
+              className="btn bg-gradient-to-r from-[#9beb9c] to-[#51ec99] rounded-3xl px-4 py-2 text-black "
+            >
+              Appointment
+            </Link>
+          ) : (
+            <Link
+              to="/signup"
+              className="btn bg-gradient-to-r from-[#9beb9c] to-[#51ec99] px-4 py-2 rounded-3xl  text-blackfont-bold"
+            >
+              Sign-up
+            </Link>
+          )}
         </div>
+        <motion.div
+          className={`md:hidden flex flex-col bg-[#afed9c]/90 text-black text-center absolute top-14 left-1/2 transform -translate-x-1/2 w-full max-w-[250px] sm:max-w-[600px] px-4 py-6 rounded-xl shadow-lg z-50 ${
+            open ? "block" : "hidden"
+          }`}
+          initial={{ y: "-100%", opacity: 0 }}
+          animate={{ y: open ? "0%" : "-100%", opacity: open ? 1 : 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+        >
+          {[
+            { name: "Home", path: "/home" },
+            { name: "About Us", path: "/about" },
+            { name: "Services", path: "/services" },
+            { name: "Doctors", path: "/Doctors" },
+            { name: "News", path: "/news" },
+            { name: "Contact", path: "/contact" },
+          ].map((item) => (
+            <Link
+              key={item.path}
+              className="py-3 text-lg font-semibold transition-colors duration-200 hover:text-white hover:bg-[#8cd574] rounded-lg"
+              to={item.path}
+              onClick={() => setOpen(false)}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </motion.div>
       </div>
-      <motion.div
-  className={`md:hidden flex flex-col bg-[#afed9c]/90 text-black text-center absolute top-14 left-1/2 transform -translate-x-1/2 w-full max-w-[250px] px-4 py-6 rounded-xl shadow-lg z-50 ${
-    open ? "block" : "hidden"
-  }`}
-  initial={{ y: "-100%", opacity: 0 }}
-  animate={{ y: open ? "0%" : "-100%", opacity: open ? 1 : 0 }}
-  transition={{ duration: 0.5, ease: "easeInOut" }}
->
-  {[
-    { name: "Home", path: "/home" },
-    { name: "About Us", path: "/about" },
-    { name: "Services", path: "/services" },
-    { name: "Doctors", path: "/Doctors" },
-    { name: "News", path: "/news" },
-    { name: "Contact", path: "/contact" },
-  ].map((item) => (
-    <Link
-      key={item.path}
-      className="py-3 text-lg font-semibold transition-colors duration-200 hover:text-white hover:bg-[#8cd574] rounded-lg"
-      to={item.path}
-      onClick={() => setOpen(false)}
-    >
-      {item.name}
-    </Link>
-  ))}
-</motion.div>
-
     </>
   );
 };
